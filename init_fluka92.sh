0be1cba52df3c3a257e88413b5953eb2e4333d2e
akiyamiyamoto <akiya.miyamoto@kek.jp>
2019-12-09 16:40:04 +0900

    python3 path was included

--- init_fluka92.sh
+++ init_fluka92.sh
@@ -4,7 +4,7 @@
 
 # . ${HOME}/soft/gcc830/init_gcc830.sh 
 
-. /sw/ilc/gcc920/setup-gcc920.sh 
+. /sw/ilc/gcc920/setup-gcc920-py3.sh 
 
 export PATH=/sw/ilc/gcc920/binutils/2.33.1/bin:${PATH}
 
@@ -12,10 +12,10 @@ export PATH=/sw/ilc/gcc920/binutils/2.33.1/bin:${PATH}
 #--------------------------------------------------------------------------------
 #     QT
 #--------------------------------------------------------------------------------
-export QTDIR="/cvmfs/ilc.desy.de/sw/x86_64_gcc49_sl6/v02-00-02/QT/4.7.4"
-export QMAKESPEC="$QTDIR/mkspecs/linux-g++"
-export PATH="$QTDIR/bin:$PATH"
-export LD_LIBRARY_PATH="$QTDIR/lib:$LD_LIBRARY_PATH"
+#export QTDIR="/cvmfs/ilc.desy.de/sw/x86_64_gcc49_sl6/v02-00-02/QT/4.7.4"
+#export QMAKESPEC="$QTDIR/mkspecs/linux-g++"
+#export PATH="$QTDIR/bin:$PATH"
+#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${QTDIR}/lib"
 
 
 
