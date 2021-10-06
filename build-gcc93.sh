4e94c12577eb79a4958e7d2dce7e20d5b9602871
akiyamiyamoto <akiya.miyamoto@kek.jp>
2021-10-06 14:23:25 +0900

    Correct build script

--- build-gcc93.sh
+++ build-gcc93.sh
@@ -3,54 +3,38 @@
 # script_dir=$(cd $(dirname $0);pwd)
 
 fluka_dir=${HOME}/fluka
+fluka_version=4-1.1
 
 cd ${fluka_dir}
 source ${fluka_dir}/init_fluka93.sh
 
-tar zxf ${fluka_dir}/orig/fluka-4-1.1.Linux-gfor9.tgz
+# ################################################
+echo "Installing FLUKA${fluka_version}"
+tar zxf ${fluka_dir}/orig/fluka-${fluka_version}.Linux-gfor9.tgz
+( cd fluka${fluka_version}/src  ;  make  )
+( cd fluka${fluka_version}/bin ; ./ldpmqmd -o flukadpm )
 
-( 
-  cd fluka-4-1.1
-  make 
-
-)
-
+# ################################################
+echo "Installing python module"
 mkdir -p ${fluka_dir}/python
+python -m pip install --user pillow numpy pydicom gdcm
 
-python -m pip install --user pillow pydicom gdcm
+# ################################################
+echo "Installing flair"
+tar xf orig/flair-3.1-14.tgz
+( cd flair-3.1 
 
+  tar xf ../orig/dicom.tgz
 
-
-cd ${FLUPRO}
-
-tar -zxf ../orig/fluka2021.2-linux-gfor64bit-10.3-AA.tar.gz
-
-make -j20
-
-cd ${FLUPRO}
-flutil/ldpmqmd
-echo "flukadpm3 was created."
-(
-  cd ${FLUPRO}/flutil
-  sed -e "s|FTOP/flukahp|FTOP/flukadpm3|g" rfluka > rflukadpm3
-  chmod +x rflukadpm3
-  echo "rflukadpm was created in `pwd`"
+  tar xf ../orig/flair-geoviewer-3.1-14.tgz
+  ln -s flair-geoviewer-3.1 geoviewer
+  ( cd flair-geoviewer-3.1 
+    make -j 4
+    make install DESTDIR=${fluka_dir}/flair-3.1
+  )
 )
-
-exit
-
-(
-tar xf ${fluka_dir}/orig/flair-3.1-14.tgz
-cd flair-3.1
-tar xf ${fluka_dir}/orig/flair-geoviewer-3.1-14.tgz
-ln -s flair-geoviewer-3.1 geoviewer
-./install.sh install . 2>&1 | tee install.log
-
-echo "flair was installed."
-)
-
-
-
+ 
+echo "Build-gcc93.sh completed." 
 
 
 
